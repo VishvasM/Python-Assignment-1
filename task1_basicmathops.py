d034c10ad599f0df0e89519d2cc8731f84a68d35
Vishvas <vishvas.mahajan@gmail.com>
2025-09-14 08:11:31 +0530

    Assignment 1

---
+++ task1_basicmathops.py
@@ -0,0 +1,8 @@
+x = int(input('Enter the first number: '))
+y = int(input('Enter the second number: '))
+x = int(x)
+y = int(y)
+print('Addition: ', x + y)
+print('Subtraction: ', x - y)
+print('Multiplication: ', x * y)
+print('Division: ', x / y)
